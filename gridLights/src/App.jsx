8c31a29d5e2ecc5a1269a5fa2155f5b04eb569b0
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-02-24 10:31:32 +0530

    done with this

--- gridLights/src/App.jsx
+++ gridLights/src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import './App.css'
 
 
-function Cell({filled, onClick})
+function Cell({filled, onClick, isDisabled})
 {
   return(
     <button type='button' 
+     disabled={isDisabled}
      onClick={onClick} 
      className={filled ? 'cell cell-activated' : 'cell'}
     />
@@ -12,23 +14,58 @@ function Cell({filled, onClick})
   )
 }
 function App() {
+
+  const [order, setOrder] = useState([])
+  const [isDeactivating, setIsDeactivating] = useState(false)
+
   const config = [
     [1,1,1],
     [1,0,1],
     [1,1,1]
   ]
 
-  const activateCells = (index) => {}
+  const deActivateCells = () => {
+    setIsDeactivating(true)
+    const timer = setInterval(() => {
+      setOrder((originalOrder) => {
+        const newOrder = originalOrder.slice()
+        newOrder.pop()
+
+        if(newOrder.length === 0){
+          clearInterval(timer)
+          setIsDeactivating(false)
+        }
+
+        return newOrder
+      })
+    }, 300);
+  }
+
+  const activateCells = (index) => {
+    const newOrder = [...order, index]
+    setOrder(newOrder);
+
+    //deActivate
+    if(newOrder.length === config.flat(1).filter(Boolean).length){
+      deActivateCells()
+    }
+  }
 
   return (
     <div className='wrapper'>
-      <div className='grid'>
+      <div 
+       className='grid'
+       style={{
+        gridTemplateColumns:`repeat(${config[0].length}, 1fr)`
+       }} 
+      >
         {config.flat(1).map((value, index) => {
-           return <Cell 
+          return value ? <Cell 
            key={index}
-           filled={false}
+           filled={order.includes(index)}
            onClick={() => activateCells(index)} 
-          />
+           isDisabled = {order.includes(index) || isDeactivating}
+          /> : <span />
         })}
       </div>
     </div>
