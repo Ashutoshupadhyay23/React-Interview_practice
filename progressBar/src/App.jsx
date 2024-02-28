73fedd92126e88a801da191bb3d7e0e727e1b2bb
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-02-28 13:47:16 +0530

    done with this scalable progressBar

--- progressBar/src/App.jsx
+++ progressBar/src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 import ProgressBar from './components/ProgressBar'
+import { useEffect } from 'react'
 
 function App() {
  
+  const [value, setValue] = useState(0)
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
+  }, [])
 
   return (
     <div className='app'>
       <span>Progress Bar</span>
-      <ProgressBar value={25} />
+
+      <ProgressBar 
+       value={value}
+
+      // to make it scalable :-> means we can provide bunch of things outside of progress bar 
+       onComplete = {() => setSuccess(true)}
+       
+      />
+
+      <span>{success ? 'Completed!' : 'Loading...'}</span>
+
     </div>
   )
 }
--- progressBar/src/components/ProgressBar.jsx
+++ progressBar/src/components/ProgressBar.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-const ProgressBar = ({value = 0}) => {
+const ProgressBar = ({value = 0, onComplete = () => {} }) => {
+
+    const [percent, setPercent] = useState(value)
+
+    useEffect(() => {
+        setPercent(Math.min(100, Math.max(value, 0)));
+
+        if(value >= 100){
+            onComplete();
+        }
+    }, [value])
+
   return (
     <div className='progress'>
-        <span> {value.toFixed()}% </span>
+        <span
+         style={{color: percent > 49 ? 'white' : 'black'}}
+        > 
+            {percent.toFixed()}% 
+        </span>
+        <div 
+        //  using width is kind of bad habit and bad for our performance. what if we suppose to not use width and you have to do with better styling which will optimize our performance
+
+        //  style={{width: `${percent}%`}}
+
+        style={{transform: `scaleX(${percent / 100})`, transformOrigin: 'left'}}
+
+        //  accessibility*
+         role='progressbar'
+         aria-valuemin={0}
+         aria-valuemax={100}
+         aria-valuenow={percent.toFixed()}
+        />
     </div>
   )
 }
 
 export default ProgressBar
+
+
+// accessibility:-> is the practice of making your website useable by as many as people as possible. we traditionally think of this as being about people with disabilities, but the practice of making sites accessible also benefits other groups such as those using mobiledevices, or those with slow network connection 
