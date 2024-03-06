0dc1fcd74eab9f2413063bb0219170b8e53a5099
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-03-06 15:08:16 +0530

    useMemo Hook

--- useMemo Hook/src/App.jsx
+++ useMemo Hook/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useMemo } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(100)
+
+  const squaredValue = () => {
+    return counter * counter;
+  }
+
+  const memoisedSquareValue = useMemo(squaredValue, [counter]);
+
 
   return (
-    <h1>useMemo Hook</h1>
+    <div className='app'>
+      <h2>Counter: {counter} </h2>
+      <h2>Squared Counter: {memoisedSquareValue} </h2>
+      <button onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+
+      <h2>Counter: {counter2} </h2>
+      <button onClick={() => setCounter2(counter2 - 1)}>
+        Decrement
+      </button>
+    </div>
   )
 }
 
