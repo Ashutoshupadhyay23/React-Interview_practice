e391586bc277be6d60afd15ac197dda3bbee7883
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-03-09 12:34:09 +0530

    useeffect hook

--- useEffect Hook/src/App.jsx
+++ useEffect Hook/src/App.jsx
@@ -1,10 +1,53 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+      if(!response.ok) {
+        throw new Error('Response is not OK')
+      }
+
+      const fetcheddata = await response.json();
+      setData(fetcheddata);
+
+    } catch (error) {
+      setError(error)
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
 
   return (
-    <h2>useEffect Hook</h2>
+    <>
+      <h1>useEffect Hook</h1>
+      <h2>useEffect how does it work..</h2>
+
+      <ul>
+        {data.map((post) => {
+          return <li key={post.id}>
+            {post.title}
+          </li>
+        })}
+      </ul>
+    </>
   )
 }
 
