34a6aeb279e283b58533c05c3bbe12fa9b0810a5
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-03-10 15:32:23 +0530

    working on UI

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,12 +1,58 @@
 import './App.css'
 
 function App() {
+
+  const checkBoxData = [
+    {title: 'Include Uppercase Letters', state: false},
+    {title: 'Include Lowercase Letters', state: false},
+    {title: 'Include Numbers', state: false},
+    {title: 'Include Symbols Letters', state: false},
+  ]
   
 
   return (
-    <>
-      <h1>Password Generator</h1>
-    </>
+    <div className='container'>
+      {/* Password text and copy */}
+      <div className='header'>
+        <div className='title'>
+          Kg372@8
+        </div>
+        <button className='copyBtn' onClick={() => {}} >
+          copy
+        </button>
+      </div>
+
+      {/* Character length  */}
+      <div className='charLength'>
+        <span>
+          <label>Character Length</label>
+          <label>4</label>
+        </span>
+
+        <input 
+         type="range"
+         min='4'
+         max='20'
+        //  value={}
+        //  onChange={}
+        />
+
+      </div>
+      {/* Checkboxes */}
+      <div className='checkboxes'>
+        {checkBoxData.map((checkbox, i) => {
+          return (
+            <div key={i}>
+              <input type="checkbox" checked={checkbox.state} />
+              <label> {checkbox.title} </label>
+            </div>
+          )
+        })}
+      </div>
+      {/* Strength */}
+      {/* Generate Button */}
+      <button className='generateBtn' onClick={() => {}}>Generate Password</button>
+    </div>
   )
 }
 
